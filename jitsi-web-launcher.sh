a3204baeaf430da4b23ccc2ddf09f025406d3437
Natanael <natanaelgplay@gmail.com>
2024-12-09 16:17:16 -0300

    Use o novo webplayer

--- jitsi-web-launcher.sh
+++ jitsi-web-launcher.sh
@@ -7,7 +7,7 @@ jitsi_url="https://meet.jit.si/$(echo ${RANDOM} ${RANDOM} ${RANDOM} | sha1sum | 
 
 function showLink() {
   echo -e "\nJunte-se a nós na reunião no Jitsi, para acessar basta clicar no link a seguir, não é necessário instalar nenhum aplicativo, tudo acontecerá pelo nevegador\n\n${jitsi_url}" | yad --wrap --margins=32 --text-info --center --borders=32 --width=640 --height=480 --text="<big><b>Quase lá para começar sua reunião</b></big>\nAgora é só copiar a mensagem e mandar para as pessoas e esperar, quando elas abrirem\no link irão entrar na reunião através do navegador\n" --button=gtk-close:1 --button="Começar reunião" --fixed --class=jitsi-launcher --name=jitsi-launcher --title="Jetsi Meet - Gerador de links" --window-icon="${HERE}/jitsi.png" && {
-    webapp-player "${jitsi_url}"
+    webapp-player "${jitsi_url}" "Jitsi Meet" "/usr/lib/tiger-os/jitsi.png"
     showLink
   }
 }
@@ -28,7 +28,7 @@ function openLink() {
   [ "${link}" = "" ] && {
     openLink "\n\nErro: O link passado está vazio"
   } || {
-    webapp-player "https://meet.jit.si/${link}"
+    webapp-player "https://meet.jit.si/${link}" "Jitsi Meet" "/usr/lib/tiger-os/jitsi.png"
   }
 }
 
