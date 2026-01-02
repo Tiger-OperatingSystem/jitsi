7ccb349346e0d383c86421e312bf6cd49bf3f265
selrahcsan <charlesstna@proton.me>
2026-01-01 21:12:30 -0300

    adicionado a função para gerar a url do jitsi com o nome da reunião

--- src/jitsi.py
+++ src/jitsi.py
@@ -1,4 +1,5 @@
 from random_word import RandomWords
+import webbrowser
 
 def gerar_nome_reuniao():
     global palavras_juntas
@@ -13,9 +14,14 @@ def gerar_nome_reuniao():
     palavras_juntas = "".join(lista_palavras)
     return palavras_juntas
 
+def url_jitsi():
+    url_base = "https://meet.jit.si/"
+    url_final = url_base + gerar_nome_reuniao()
+    return url_final
+
 def main():
-    # Teste para gerar o nome da reunião
-    print(gerar_nome_reuniao())
+    # Teste para gerar a url da reunião
+    print(url_jitsi())
     
 if __name__ == '__main__':
     main()
